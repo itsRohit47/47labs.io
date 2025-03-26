17fcfff38a194560e9442272d7df39af660e9831
Rohit Bajaj <rohitbajaj13@yahoo.com>
2025-03-27 00:34:03 +1100

    shgsh

--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
             >
               {loading ? (
                 <div className="flex items-center justify-center">
-                  <div className="animate-spin rounded-full h-4 w-4 border-t-4 border-black/80 border"></div>
+                  <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-black/80 border"></div>
                 </div>
               ) : (
                 'Join Waitlist'
