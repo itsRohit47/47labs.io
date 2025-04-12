027833d110938d48ae342da9b2469f3c99fd70c5
Rohit Bajaj <rohitbajaj13@yahoo.com>
2025-04-12 11:45:59 +1000

    abosuy

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,46 +2,43 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 'use client';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 export default function About() {
     return (
-        <motion.div className="h-full"
+        <motion.div className="h-full max-w-3xl mx-auto space-y-4 w-full pt-20 pb-44 flex flex-col"
             initial={{ opacity: 0, filter: 'blur(20px)' }}
             animate={{ opacity: 1, filter: 'blur(0px)' }}
             transition={{ duration: 0.5 }}>
 
-            <div className='max-w-3xl mx-auto px-4 space-y-4 w-full h-full py-16 pb-44'>
-                <div className='flex items-center justify-between'>
-                    <a href='/' className="text-white/50 hover:text-white/100 text-sm flex items-center ">
-                        <ChevronLeftIcon className="inline-block w-5 h-5" /> Back to Home
-                    </a>
-                    <a href='/' className="text-white/50 hover:text-white/100 text-sm flex items-center ">
-                        The Team <ChevronRightIcon className="inline-block w-5 h-5" />
-                    </a>
-                </div>
-                <h1 className="text-2xl lg:text-4xl font-thin ">
-                    47Labs is a <span className='font-serif font-semibold text-white'>Hacker Lab</span> where we build cool stuff, mostly related to solving difficult technology problems using Artificial Intelligence and software!
-                </h1>
-                <p className="">
-                    At its core, 47Labs is a software company that focuses on building cool and innovative projects. Its essentially an experiment lab to try and solve difficult Digital/Technical problems with the porwer of software. Few topic we are interested in are as follows:
-                </p>
-                <p className="">
-                    Impact of Quantam computing and Artificial Intelligence on Security of Software, Effectiveness of AI in Software Development, AI-Powered Software Development.
-                </p>
-                <p className="">
-                    <img src="/logo.png" alt="The Theory" className="w-full h-80 object-cover mx-auto" />
-                </p>
-                <p className="">
-                    We are also interested in building a community of like-minded individuals who are interested in the same topics. We believe that by working together, we can achieve more than we could alone.
-                </p>
-                <p className="">
-                    In the lab at the moment we are working on to build a product called picklock, a security platform for early stage startups. The platform is designed to help startups secure their software and data from the ground up. We are also working on a few other projects that we will be announcing soon.
-                </p>
-                <img src="/picklock.jpeg" alt="The Theory" className="w-full h-auto object-cover mx-auto" />
-                <p>
-                    To be continued...
-                </p>
+            <div className='flex items-center justify-between gap-x-4 w-full'>
+                <a href='/' className="text-white/50 hover:text-white/100 text-xs flex items-center ">
+                    <ChevronLeftIcon className="inline-block w-5 h-5" /> Back Home
+                </a>
+                <a href='team' className="text-white/50 hover:text-white/100 text-xs flex items-center ">
+                    The Team <ChevronRightIcon className="inline-block w-5 h-5" />
+                </a>
             </div>
+            <h1 className="text-2xl lg:text-4xl font-thin w-full ">
+                47Labs is a <span className='font-serif font-semibold text-white'>Hacker Lab</span> where we build cool stuff, mostly related to solving difficult technology problems using Artificial Intelligence and software!
+            </h1>
+            <p className="w-full">
+                At its core, 47Labs is a software company that focuses on building cool and innovative projects. Its essentially an experiment lab to try and solve difficult Digital/Technical problems with the porwer of software. Few topic we are interested in are as follows:
+            </p>
+            <p className="w-full">
+                Impact of Quantam computing and Artificial Intelligence on Security of Software, Effectiveness of AI in Software Development, AI-Powered Software Development.
+            </p>
+            <Image src={"/logo.png"} alt="The Theory" className="max-w-3xl h-80 mx-auto object-cover " width={1000} height={1000} />
+            <p className="w-full">
+                We are also interested in building a community of like-minded individuals who are interested in the same topics. We believe that by working together, we can achieve more than we could alone.
+            </p>
+            <p className="w-full">
+                In the lab at the moment we are working on to build a product called picklock, a security platform for early stage startups. The platform is designed to help startups secure their software and data from the ground up. We are also working on a few other projects that we will be announcing soon.
+            </p>
+            <Image src={"/picklock.jpeg"} alt="The Theory" className="max-w-3xl h-auto mx-auto object-cover " width={1000} height={1000} />
+            <p>
+                To be continued...
+            </p>
         </motion.div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,31 +28,30 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-primary`}
       >
         {children}
-        <div className="fixed bottom-0 left-0 w-full h-8 blur-2xl bg-gradient-to-r from-transparent to-green-500 opacity-40"></div>
-        <div className="text-white/50 fixed flex space-x-4 bottom-4 left-1/2 -translate-x-1/2 flex-wrap justify-center text-xs items-center w-full mx-auto max-w-80 lg:max-w-3xl">
-          <a
-            href="team"
-            className="text-white/50 hover:text-white/100"
-          >
-            The Team
-          </a>
+        <div className="text-white fixed flex space-x-4 bottom-0 right-0 pb-2 pt-4 justify-center text-xs items-center w-full mx-auto px-4 z-20 bg-gradient-to-t from-black/90 from-30% to-transparent">
           <a
             href="about"
-            className="text-white/50 hover:text-white/100"
+            className="text-primary hover:text-white/100"
           >
             About 47Labs
           </a>
           <a
+            href="team"
+            className="text-primary hover:text-white/100"
+          >
+            The Team
+          </a>
+          <a
             href="https://discord.gg/bb3XTpjcR3"
             target="_blank"
-            className="text-white/50 hover:text-white/100"
+            className="text-primary hover:text-white/100"
           >
             Join the Community
           </a>
           <a
             href="https://x.com/fortysevenlabs"
             target="_blank"
-            className="text-white/50 hover:text-white/100"
+            className="text-primary hover:text-white/100"
           >
             Folllow 47Labs on X
           </a>
