82383e4178dbdb71af77d8ee0a8836c80aa9de08
Rohit Bajaj <rohitbajaj13@yahoo.com>
2025-04-19 01:14:39 +1000

    gsjhgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,14 +24,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full" data-theme="light">
+      <head>
+        <Script id="theme-script" strategy="beforeInteractive">
+          {`
+            // On page load or when changing themes, best to add inline in head to avoid FOUC
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
+            }
+          `}
+        </Script>
+      </head>
       <body
-        style={{
-          position: 'relative',
-        }}
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-primary max-w-2xl mx-auto p-4 `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-primary max-w-2xl mx-auto p-4 dark:bg-gray-900 dark:text-white min-h-dvh h-full`}
       >
-        {children}
+        <div className="h-full flex flex-col">
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,41 @@
 'use client'
 import { motion } from "framer-motion"
 import { useState, useEffect } from "react";
-import { HomeIcon, BoxIcon, PencilIcon, SendIcon, ArrowUpRightIcon } from "lucide-react";
+import { HomeIcon, BoxIcon, PencilIcon, SendIcon, ArrowUpRightIcon, MoonIcon, SunIcon } from "lucide-react";
+
+// Theme toggle component
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Initialize theme state based on localStorage or system preference
+    setIsDarkMode(
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = isDarkMode ? "light" : "dark";
+    localStorage.theme = newTheme;
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+    setIsDarkMode(newTheme === "dark");
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-md hover:bg-gray-100 dark:hover:bg-white/10 transition-colors fixed top-2 cursor-pointer right-4"
+      aria-label="Toggle dark mode"
+    >
+      {isDarkMode ? (
+        <SunIcon className="h-5 w-5 text-yellow-400" strokeWidth={1.5} />
+      ) : (
+        <MoonIcon className="h-5 w-5 text-gray-600" strokeWidth={1.5} />
+      )}
+    </button>
+  );
+};
 
 const countries = [
   { name: "United States", emoji: "🇺🇸" },
@@ -24,7 +58,9 @@ export default function Home() {
   const [name, setName] = useState("");
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
+  const [loading2, setLoading2] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [invalidEmail, setInvalidEmail] = useState(false);
 
   useEffect(() => {
     const hasJoined = localStorage.getItem('hasJoined');
@@ -40,11 +76,11 @@ export default function Home() {
 
   const handleSubmit = async () => {
     if (!isValidEmail(email)) {
-      alert('Please enter a valid email address');
+      setInvalidEmail(true);
       return;
     }
 
-    setLoading(true);
+    setLoading2(true);
     try {
       const response = await fetch('/api/submit', {
         method: 'POST',
@@ -68,7 +104,7 @@ export default function Home() {
       console.error(error);
       alert('Failed to join waitlist. Please try again.');
     } finally {
-      setLoading(false);
+      setLoading2(false);
     }
   };
 
@@ -80,38 +116,84 @@ export default function Home() {
   ];
 
   const products = [
-    { name: "PickLock", description: "Bug Bounty Marketplace for AI Era", href: "https://picklock.47labs.io", icon: "/picklock.png" },
     { name: "47Labs", description: "Home to all of 47labs products", href: "https://47labs.io", icon: "/logo.png" },
-    { name: "Techfolio", description: "No-code portfolio builder for tech professionals", href: "https://techfolio.gg", icon: null },
+    { name: "PickLock", description: "Bug Bounty Marketplace for AI Era", href: "https://picklock.47labs.io", icon: "/picklock.png" },
+    { name: "Techfolio", description: "No-code portfolio builder for techies", href: "https://techfolio.gg", icon: null },
+    { name: "10x Next.js Starter Template", description: "Next.js (app) + tRPC + prisma + next-auth", href: "https://github.com/itsRohit47/10xdev-next", icon: null },
+  ]
+
+  const writings = [
+    { title: "CS50 - Introduction to Artificial Intelligence using Python by HarvardX", href: "https://www.notion.so/CS50AI25-Notes-1b572b3003aa80889315fd68376b26e8?pvs=4", category: "Notes" },
+    { title: "SQL Notes - Good to go over these in case you have an interview or something", href: "https://www.notion.so/SQL-Notes-1d772b3003aa807cbd3aeefe1f5daa0c", category: "Notes" },
+    { title: "Data Structures and Algorithms", href: "https://www.notion.so/DSA-Notes-1b272b3003aa801284d4f89374b1a60a?pvs=4", category: "Notes" },
+    { title: "Web Development - advanced stuff, a level above basics", href: "https://www.notion.so/DSA-Notes-1b272b3003aa801284d4f89374b1a60a?pvs=4", category: "Notes" },
+    { title: "This is what i would use to start from basics of development side of technology", href: "https://www.notion.so/Dev-Resources-KickStarter-0-1-1c572b3003aa80718759dfc1749abd54?pvs=4", category: "Resources" },
+    { title: "List of raw ideas, captured here and there, could become products, not sure", href: "https://www.notion.so/Raw-Ideas-13f72b3003aa80349d92d4280ee3a667?pvs=4", category: "Dumps/Ideas/Tips" },
+    { title: "Lessons from building my first product, trying to sell it, and avoiding costly ($$$) mistakes", href: "https://www.notion.so/Lessons-from-Building-My-First-Product-1d972b3003aa80b1bda4f04e1be3f3b9?pvs=4", category: "Dumps/Ideas/Tips" },
   ]
 
   //effect to set loaded to true after 2 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
   const [currentLink, setCurrentLink] = useState(links[0].name);
   return (
-    <>
+    <div className="flex flex-col w-full h-full ">
       {loading ? (
         <motion.div
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           transition={{ duration: 0.5, delay: 2 }}
-          className="flex items-center justify-center h-screen"
+          className="flex items-center justify-center h-full gap-x-3"
         >
-          <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-white/80 border"></div>
+          <div>
+            <h1 className=" dark:text-white text-lg font-semibold">
+              <span className="">47</span><span className="dark:text-white/70 text-black/70">Labs</span>
+            </h1>
+            <p className="dark:text-white/70 text-black/70">
+              Build <span>•</span> Ship <span>•</span> Scale <span>•</span> Repeat
+            </p>
+            <svg
+              width="100"
+              height="20"
+              viewBox="0 0 100 20"
+              className="mt-2"
+            >
+              {[...Array(10)].map((_, i) => (
+                <motion.rect
+                  key={i}
+                  x={i * 11}
+                  y={0}
+                  width="8"
+                  height="20"
+                  fill="currentColor"
+                  className="dark:text-white/70 text-black/70"
+                  initial={{ scaleY: 0.5 }}
+                  animate={{
+                    scaleY: [0.5, 1, 0.5],
+                    transition: {
+                      duration: 1,
+                      repeat: Infinity,
+                      delay: i * 0.1
+                    }
+                  }}
+                />
+              ))}
+            </svg>
+          </div>
         </motion.div>
       ) : (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
-          className="pb-32"
+          className="relative flex flex-col h-full "
         >
-          <div className="flex flex-col w-full text-sm font-light justify-center">
+          <ThemeToggle />
+          <div className="flex flex-col w-full text-sm font-light">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
@@ -120,17 +202,23 @@ export default function Home() {
             >
               <div className="flex">
                 <div className="flex flex-col w-full">
-                  <h1 className=" text-white text-lg">
-                    <span className="">47</span><span className="text-white/70">Labs</span>
+                  <div className="flex items-center gap-x-2">
+                    {/* <img src={'logo.png'} alt={'logo'} className="w-12 h-12 object-cover rounded-sm" /> */}
+                    <div>
+                      <h1 className=" dark:text-white text-lg font-semibold">
+                        <span className="">47</span><span className="dark:text-white/70 text-black/70">Labs</span>
+                      </h1>
+                      <p className="dark:text-white/70 text-black/70">
+                        Build <span>•</span> Ship <span>•</span> Scale <span>•</span> Repeat
+                      </p>
+                    </div>
+                  </div>
+
+                  <h1 className="text-3xl lg:text-6xl dark:text-white  mt-10">
+                    <span className="font-row-2">Hacker Lab</span> <span className="dark:text-white/80 font-thin text-xl">Based in Melbourne, Australia!</span>
                   </h1>
-                  <p className="text-white/70">
-                    Build <span>•</span> Ship <span>•</span> Scale <span>•</span> Repeat
-                  </p>
-                  <h1 className="text-3xl lg:text-6xl text-white font-row-2 mt-10">
-                    <span className="">Hacker Lab</span> <span className="text-white/80 font-semibold text-xl font-row">based in Melbourne, Australia!</span>
-                  </h1>
-                  <p className="text-white/70 text-sm mt-4">
-                    47 labs is my personal hacking lab where i try out new ideas and build products that i think can help the community. I am a solo hacker and i love to build things that can help others.
+                  <p className="dark:text-white/70 text-black text-sm mt-4">
+                    47 labs is my (<a href="https://www.linkedin.com/in/itsrohitbajaj/" target="_blank" className="dark:text-white/70 dark:hover:text-white hover:underline transition-colors duration-300">@row</a>) digital home for my projects, ideas, and experiments. I like to learn, write, share, and build things. Join the waitlist to get notified when I launch something ✨
                   </p>
                 </div>
               </div>
@@ -142,41 +230,103 @@ export default function Home() {
               transition={{ duration: 0.5, delay: 0.3 }}
               className="flex flex-col space-y-4 mb-10"
             >
-              <div className="gap-3 flex flex-col">
+              <h3 className="text-xs font-medium dark:text-white/90">All Products</h3>
+              <div className="gap-3 grid grid-cols-2">
                 {products.map((product, index) => (
                   <motion.div
                     key={product.name}
-                    initial={{ opacity: 0, x: -20 }}
-                    animate={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.3, delay: 0.6 + (index * 0.1) }}
                     onClick={() => window.open(product.href, '_blank')}
-                    className="flex group cursor-pointer rounded-lg p-4 items-center gap-x-4 border border-white/10 hover:bg-white/2 transition-colors duration-300"
+                    className="flex group cursor-pointer rounded-lg p-4 items-center gap-x-4 border border-gray-200 hover:bg-gray-100 dark:border-white/10 dark:hover:bg-white/2 transition-colors duration-300"
                   >
-                    {product.icon ? (
-                      <img src={product.icon} alt={product.name} className="w-10 h-10 object-cover rounded-sm" />
-                    ) : (
-                      <BoxIcon className="w-10 h-10 text-white/80 p-2 bg-white/10 rounded-sm" strokeWidth={1.5} />
-                    )}
                     <div>
                       <h3 className="text-sm">{product.name}</h3>
-                      <p className="text-white/70 text-xs">{product.description}</p>
+                      <p className="dark:text-white/70 text-black/70 text-xs">{product.description}</p>
                     </div>
-                    <ArrowUpRightIcon className="h-5 w-5 text-white/70 ml-auto group-hover:text-white group-hover:translate-x-0 -translate-x-1 group-hover:scale-110 transition-all duration-300" />
+                    <ArrowUpRightIcon className="h-5 w-5 dark:text-white/70 text-black/70 ml-auto dark:group-hover:text-white group-hover:translate-x-0 -translate-x-1 group-hover:scale-105 transition-all duration-300" />
                   </motion.div>
                 ))}
               </div>
             </motion.div>
-
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.8 }}
+              className="flex flex-col space-y-6 mb-10"
+            >
+              {Object.entries(writings.reduce((acc, writing) => {
+                // Group writings by category
+                acc[writing.category] = acc[writing.category] || [];
+                acc[writing.category].push(writing);
+                return acc;
+              }, {} as Record<string, typeof writings>)).map(([category, items], categoryIndex) => (
+                <div key={category} className="space-y-2">
+                  <h3 className="text-xs font-medium dark:text-white/90">{category}</h3>
+                  {items.map((writing, index) => (
+                    <motion.div
+                      key={writing.title}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{ duration: 0.3, delay: 0.8 + (categoryIndex * 0.1) + (index * 0.05) }}
+                      className="flex group cursor-pointer rounded-lg items-center gap-x-4 transition-colors duration-300"
+                    >
+                      <a href={writing.href} target="_blank" className="dark:text-white/70 text-black/70 text-sm hover:underline flex gap-x-1 items-center transition-all duration-300">{writing.title}<ArrowUpRightIcon className="h-4 w-4 dark:text-white/70 text-black/70 ml-auto dark:group-hover:text-white opacity-0  group-hover:opacity-100 transition-opacity duration-300" />
+                      </a>
+                    </motion.div>
+                  ))}
+                </div>
+              ))}
+            </motion.div>
+            {/* email collect */}
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 1 }}
+              className="flex flex-col space-y-2 mb-10"
             >
-              <h3 className="text-xs text-white/70">Built with ❤️ by <a href="https://www.linkedin.com/in/itsrohitbajaj/" target="_blank" className="text-white/70 hover:text-white transition-colors duration-300">@row</a></h3>
+              <h3 className="text-xs font-medium dark:text-white/90">Join the waitlist</h3>
+
+              {success ? (
+                <p className="text-sm mt-2">Thanks for joining the waitlist! 🎉</p>
+              ) : <div className="flex flex-col space-y-2">
+                <div className="flex items-center gap-x-3 text-sm relative">
+                  <div className="flex flex-col space-y-2 w-full items-center justify-center">
+                    <input
+                      type="text"
+                      placeholder="Your email address"
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                        setInvalidEmail(e.target.value !== '' && !isValidEmail(e.target.value));
+                      }}
+                      className={`border ${invalidEmail ? 'border-red-500' : 'border-gray-200 dark:border-white/10'} dark:bg-white/5 bg-gray-50 rounded-md p-2 w-full focus:outline-none focus:ring-1 focus:ring-gray-300 dark:focus:ring-white/10 transition-all duration-150`}
+                    />
+                  </div>
+
+                  <button
+                    onClick={handleSubmit}
+                    disabled={loading2 || success}
+                    className={`bg-primary dark:text-black text-white rounded-md px-4 py-2 ${loading2 || success ? 'opacity-50 cursor-not-allowed' : 'hover:bg-primary/80 transition-colors text-nowrap duration-300'}`}
+                  >
+                    {loading2 ? 'Loading...' : success ? 'Joined!' : 'Join the waitlist'}
+                  </button>
+                </div>
+              </div>}
             </motion.div>
-          </div>
-        </motion.div>
-      )}
-    </>
+            <motion.div
+              initial={{ opacity: 0, filter: "blur(20px)" }}
+              animate={{ opacity: 1, filter: "blur(0px)" }}
+              transition={{ duration: 0.5, delay: 1 }}
+              className=""
+            >
+              <h3 className=" text-xs dark:text-white/70 text-black/70 mb-32">Built with ❤️ by <a href="https://www.linkedin.com/in/itsrohitbajaj/" target="_blank" className="dark:text-white/70 dark:hover:text-white hover:underline transition-colors duration-300">@row</a></h3>
+            </motion.div>
+          </div >
+        </motion.div >
+      )
+      }
+    </div >
   );
 }
